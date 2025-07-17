46ccbcd2cc278b2a5394b1b7cf39f27a727b0a2a
Carlosvleon <vallejos.leon.carlos@gmail.com>
2025-07-16 23:51:02 -0400

    este es el commit que busco

--- backend-ferreteria/src/db/index.js
+++ backend-ferreteria/src/db/index.js
@@ -6,6 +6,7 @@ console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '***' : 'NOT SET');
 console.log('DB_PORT:', process.env.DB_PORT);
 console.log('DB_NAME:', process.env.DB_NAME);
+ 
 
 
 const pool = new Pool({
